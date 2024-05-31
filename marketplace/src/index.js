f435fd242c0becc83ec878c7c1c234ef1c39c77d
Evandro Mendes <evandromende@gmail.com>
2024-05-31 17:21:58 -0300

    Feat: criando rotas

--- marketplace/src/index.js
+++ marketplace/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './page/Home';
+import Login from './page/Login';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // BrowserRouter - responsavel pelo roteamento do browser e gerenciar rotas
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ marketplace/src/page/Login/index.js
@@ -0,0 +1,7 @@
+const Login = () =>{
+    return (
+        <h1>Minha Tela de Login</h1>
+    )
+}
+
+export default Login;
